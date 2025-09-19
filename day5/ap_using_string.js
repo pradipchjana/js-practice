7c7ff8da8ac54bcf1b358d30e551bc919802da1f
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-09-19 14:04:47 +0530

    Create file for ap using string

---
+++ day5/ap_using_string.js
@@ -0,0 +1,15 @@
+// console.log("hello");
+let term = ".....";
+let apString = "";
+let counter = ""
+while (counter !== term) {
+  counter = counter + "."
+  apString = apString + counter;
+  
+}
+// console.log(counter);
+// console.log(counter.length);
+// console.log(term);
+// console.log(term.length);
+// console.log(apString);
+console.log(apString.length);
