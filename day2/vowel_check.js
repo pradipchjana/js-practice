285161ec615d98415255f86529fdbfe5584d5f0a
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-09-19 14:16:34 +0530

    Create file for vowel check

---
+++ day2/vowel_check.js
@@ -0,0 +1,22 @@
+let alphabets = "a";
+
+switch(alphabets){
+  case "a":
+    console.log(alphabets,"is vowel");
+    break;
+  case "e":
+    console.log(alphabets,"is vowel");
+    break;
+  case "i":
+    console.log(alphabets,"is vowel");
+    break;
+  case "o":
+    console.log(alphabets,"is vowel");
+    break;
+  case "u":
+    console.log(alphabets,"is vowel");
+    break;
+  default:
+    console.log(alphabets,"consonant");
+    break;
+}
