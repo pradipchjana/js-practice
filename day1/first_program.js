351ee88485b59e70d61a33a8f35ef2223895f022
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-09-19 12:31:58 +0530

    Create first program in js

---
+++ day1/first_program.js
@@ -0,0 +1,8 @@
+let biriyani = 130;
+const iHaveMoney = 120;
+console.log("Hi i want to eat biriyani");
+if(iHaveMoney <= biriyani) {
+    console.log("i dont have money");
+} else {
+    console.log("yeah, i have enough money");
+}
