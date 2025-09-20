62980b0a9c72567dce6222140ff84f8e61d6d4c0
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-09-20 14:02:54 +0530

    identify the location of zebra and lion

---
+++ assignment_lion_hunt/find_lion_and_zebra.js
@@ -0,0 +1,21 @@
+//for Identify where zebra and lion is located
+const lionAndZebraPosition = "L Z";
+
+let lion = "L";
+let zebra = "Z";
+for (let zebraFinder = 0; zebraFinder < lionAndZebraPosition.length; zebraFinder++) {
+
+   if(lionAndZebraPosition[zebraFinder] === zebra){
+        console.log("zebra in",zebraFinder);
+
+      }
+    
+}
+
+for (let lionFinder = 0; lionFinder < lionAndZebraPosition.length; lionFinder++) {
+
+   if(lionAndZebraPosition[lionFinder] === lion){
+        console.log("Lion in",lionFinder);
+        
+      }
+}
