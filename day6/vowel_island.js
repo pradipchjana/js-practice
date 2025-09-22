2a36b5600d0ba9f9f42679ad353d31c2fd550609
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-09-22 10:18:55 +0530

    for contiguous vowel island

--- day6/vowel_island.js
+++ day6/vowel_island.js
@@ -1,10 +1,15 @@
-let meadow = "boom";
 let vowelIsland = 0;
-for (let indexOfMeadow = 0; indexOfMeadow < meadow.length; indexOfMeadow++) {
+let meadow = "queue";
+for (let indexOfMeadow = 0; indexOfMeadow < meadow.length ; indexOfMeadow++) {
   
   if(meadow[indexOfMeadow]=== "a" || meadow[indexOfMeadow]=== "e" || meadow[indexOfMeadow]=== "i" || meadow[indexOfMeadow]=== "o" || meadow[indexOfMeadow]=== "u"){
-    vowelIsland = vowelIsland + 1;
+    //console.log("Vowel islands:1",meadow[indexOfMeadow]);
+    if(meadow[indexOfMeadow + 1]!== "a" && meadow[indexOfMeadow + 1] !== "e" && meadow[indexOfMeadow + 1]!== "i" && meadow[indexOfMeadow + 1]!== "o" && meadow[indexOfMeadow + 1]!== "u"){
+      
+      //console.log("Vowel islands:",meadow[indexOfMeadow]);
+      vowelIsland = vowelIsland + 1;
+    }
   }
   
 }
-console.log("Vowel islands:",vowelIsland);
+console.log("Vowel island:",vowelIsland);
