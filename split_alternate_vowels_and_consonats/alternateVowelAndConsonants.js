93bdb704ab14d4019020e894bbd6c7b623d88443
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-09-27 16:34:44 +0530

    Create alternateVowelAndConsonants.js

---
+++ split_alternate_vowels_and_consonats/alternateVowelAndConsonants.js
@@ -0,0 +1,21 @@
+function splitAWord(string) {
+
+}
+
+function display(actual, string, expected) {
+  const check = actual === expected ? '✅' : '❌';
+  const inputFragment = "[" + string + "]";
+  const expectedFragment = "Expected: " + expected;
+  const actualFragment = 'Actual: ' + actual;
+  const message = check + inputFragment + expectedFragment + actualFragment;
+  console.log(message);
+}
+
+function testSplitAWord(string, expected) {
+  const actual = splitAWord(string);
+  display(actual, string, expected);
+}
+
+function testAll() {
+  testSplitAWord('a', 'a');
+}
