530fc2d9f1c5b9d364edda47667daeb1248668be
pradipchjana <167102394+pradipchjana@users.noreply.github.com>
2025-10-10 16:05:00 +0530

    Update guessNumber.js

--- games/guessNumber.js
+++ games/guessNumber.js
@@ -1,1 +1,31 @@
+function generateNumber() {
+  return Math.floor(Math.random() * 100);
+}
 
+function guessNumber(number, noOfChance = 5) {
+  const choiceString = prompt("Enter your number(1-100)");
+  const choice = parseInt(choiceString);
+  if (noOfChance === 0) {
+    console.log("You Loose!");
+    return;
+  }
+  if (choice === number) {
+    console.log("Yeah,You Win");
+    return;
+  }
+  if (choice > number) {
+    console.log("Too high");
+  }
+  guessNumber(number, noOfChance - 1);
+}
+
+function findNumber() {
+  const randomNumber = generateNumber();
+  guessNumber(randomNumber);
+}
+
+function play() {
+  findNumber();
+}
+
+play();
