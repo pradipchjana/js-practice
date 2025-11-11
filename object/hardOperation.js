d3da16b2b72c55d14404b78b85676fae1f2aef1f
Pradip Jana <janaprdip501@gmail.com>
2025-11-11 18:50:22 +0530

    check fill system

--- object/hardOperation.js
+++ object/hardOperation.js
@@ -1,3 +1,5 @@
 export const factorial = x => x <= 1 ? 1 : x * factorial(x - 1);
 
 export const power = (x, y) => x ** y;
+
+export let SOME_CONSTANT = 5;
--- object/test_case.js
+++ object/test_case.js
@@ -1,11 +1,14 @@
+import { isEven } from "./check.js";
+import { factorial, SOME_CONSTANT } from "./hardOperation.js";
+import { add } from "./operation.js";
 
-
+console.log(SOME_CONSTANT);
 
 const test = (testCase) => {
   const actual = testCase.funName(...testCase.fnArgs);
   const expectedValue = testCase.expected;
   const isPassed = actual === expectedValue;
-  const symbol = isPassed ?"✅" : "❌";
+  const symbol = isPassed ? "✅" : "❌";
   console.log(`${symbol}---->${testCase.des}`);
   if (!isPassed) {
     console.log(...testCase.fnArgs);
