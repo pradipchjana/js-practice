b8043dd121522372c6a0195e45ae37ef8dfd4d4b
Pradip Chandra Jana <pradipchandrajana@Pradips-MacBook-Pro.local>
2025-11-11 09:54:47 +0530

    code for merge sort

--- sorting/merged_sort.js
+++ sorting/merged_sort.js
@@ -10,4 +10,24 @@ const mergeSort = array => {
   return [left, right];
 }
 
-console.log(mergeSort([1,2,3,4,5]));
+const merge = (left, right) => {
+  let sorted = [];
+  let i = 0;
+  let j = 0;
+  
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      sorted.push(left[i]);
+      i++;
+    } else {
+      sorted.push(right[j]);
+      j++;
+    }
+  }
+
+  return sorted.concat(left.slice(i)).concat(right.slice(j));
+}
+
+
+console.log(mergeSort([1, 2, 3, 4, 5]));
+console.log(merge([3, 5, 7], [4, 6, 8]));
