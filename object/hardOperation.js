3b8a5d4ad5315d81e68bae349b33669fed23492e
Pradip Jana <janaprdip501@gmail.com>
2025-11-12 09:33:43 +0530

    learn object and make testcase file for those

--- object/hardOperation.js
+++ object/hardOperation.js
@@ -2,4 +2,5 @@ export const factorial = x => x <= 1 ? 1 : x * factorial(x - 1);
 
 export const power = (x, y) => x ** y;
 
-export let SOME_CONSTANT = 5;
+export const SOME_CONSTANT = 5;
+export const SOME_ALPHABET = "abc";
--- object/test_case.js
+++ object/test_case.js
@@ -1,15 +1,39 @@
 import { isEven } from "./check.js";
-import { factorial, SOME_CONSTANT } from "./hardOperation.js";
+import { factorial, SOME_CONSTANT, SOME_ALPHABET } from "./hardOperation.js";
 import { add } from "./operation.js";
 
-console.log(SOME_CONSTANT);
+const dbg = x => {
+  console.log(x);
+  return x;
+}
+
+const testCase = [
+  {
+    des: "add 1 and 2",
+    funName: add,
+    fnArgs: [1, 4],
+    expected: 5,
+  },
+  {
+    des: "isEven 5",
+    funName: isEven,
+    fnArgs: [5],
+    expected: false,
+  },
+  {
+    des: "isEven 5",
+    funName: factorial,
+    fnArgs: [5],
+    expected: 120,
+  }
+];
 
 const test = (testCase) => {
   const actual = testCase.funName(...testCase.fnArgs);
   const expectedValue = testCase.expected;
   const isPassed = actual === expectedValue;
   const symbol = isPassed ? "✅" : "❌";
-  console.log(`${symbol}---->${testCase.des}`);
+  console.log(`${symbol} ----> ${testCase.des}`);
   if (!isPassed) {
     console.log(...testCase.fnArgs);
     console.log({ actual, expectedValue });
@@ -17,13 +41,9 @@ const test = (testCase) => {
 };
 
 const main = () => {
-  const testCase = {
-    des: "add 1 and 2",
-    funName: add,
-    fnArgs: [1, 4],
-    expected: 5,
-  };
-  test(testCase);
+  for (const tests of testCase) {
+    test(tests);
+  }
 };
 
 main();
