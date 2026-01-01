44db05b06a8b152692b4f842698a173c8cf0c5e2
Pradip Jana <janaprdip501@gmail.com>
2026-01-01 22:13:59 +0530

    add type of task

--- js/promise_practice/maggieReceipe/doJob.js
+++ js/promise_practice/maggieReceipe/doJob.js
@@ -4,20 +4,28 @@ const task = async (file, time) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       const end = Date.now();
-      resolve({ instruction,start,end,time});
+      resolve({ instruction, start, end, time });
     }, time);
   }).then(console.log, console.log);
 };
 
-const process = tasks => tasks.reduce(
-  (p, ta) => p.then((_) => Promise.all(ta.map((t) => { const [file, time] = t.split("/");return task(file, time) }))),
-  Promise.resolve(),
-);
+const process = (tasks) =>
+  tasks.reduce(
+    (p, ta) =>
+      p.then((_) =>
+        Promise.all(ta.map((t) => {
+          const [file, time] = t.split("/");
+          return task(file, time);
+        }))
+      ),
+    Promise.resolve(),
+  );
 
 const main = () => {
-  Deno.readTextFile("maggie.txt").then(rawData => rawData.split("\n").map(x=> x.split(",")))
+  Deno.readTextFile("maggie.txt").then((rawData) =>
+    rawData.split("\n").map((x) => x.split(","))
+  )
     .then((tasks) => process(tasks));
 };
-
 
 main();
--- js/promise_practice/task.js
+++ js/promise_practice/task.js
@@ -1,29 +1,29 @@
-const task = (des, time,failProb = 0) => {
+const task = (des, time,type,failProb = 0) => {
   const start = Date.now();
   return new Promise((resolve,reject) => {
     setTimeout(() => {
       const end = Date.now();
       const isFailed = Math.random() < failProb;
       if (isFailed) {
-        reject({ des, start, end, duration: end - start,isFailed })
+        reject({ des, start, end, duration: end - start,isFailed,type })
       }
-      resolve({ des, start, end, duration: end - start,isFailed });
+      resolve({ des, start, end, duration: end - start,isFailed ,type});
     }, time);
   }).then(console.log,console.log);
 };
 
 const TASK = {
-  "task1": () => task("task1", 3000),
-  "task2": () => task("task2", 2000,0.5),
-  "task3": () => task("task3", 1000, 1.0),
-  "task4": () => task("task4", 4000, ),
-  "task5": () => task("task5", 3000,),
+  "task1": (type) => task("task1", 3000,type),
+  "task2": (type) => task("task2", 2000,type,0.5),
+  "task3": (type) => task("task3", 1000,type, 1.0),
+  "task4": (type) => task("task4", 4000,type ),
+  "task5": (type) => task("task5", 3000,type),
 };
 
 const tasks = [["task1"], ["task2"], ["task3", "task4"], ["task5"]];
 
 const main= (tasks) => {
-  tasks.reduce((p, ta) => p.then((_) => Promise.all(ta.map((t)=> TASK[t]()))), Promise.resolve());
+  tasks.reduce((p, ta) => p.then((_) => { const type = ta.length < 2?"serial" :"parralal";return Promise.all(ta.map((t) => TASK[t](type))) }), Promise.resolve());
 }
 
 main(tasks);
