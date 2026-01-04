a915a128b4b677aaee646941460d31ae62a5f15a
Pradip Jana <janaprdip501@gmail.com>
2026-01-04 15:55:32 +0530

    valid rowfor 2d array

--- games/suduku/src/suduku_validation.js
+++ games/suduku/src/suduku_validation.js
@@ -1,10 +1,22 @@
 const dbg = value => console.log(value) || value;
-export const validRow = (input) => {
-  const isPresent = Array.from({length: input.length },()=> 0)
-  for (let x = 0; x < input.length; x++) {
-    if (input[x] === 0) continue;
-    isPresent[input[x]]++;
+
+const input = [
+  [1,2,3],
+  [2,3,1],
+  [3,1,2],
+]
+
+export const validRow = (grid,y) => {
+  const isPresent = Array.from({ length: grid[0].length }, () => 0);
+  
+  for (let x = 0; x < grid.length; x++) {
+    if (grid[y][x] === 0) continue;
+    isPresent[grid[y][x] - 1]++;
+    if (isPresent[grid[y][x] - 1] > 1) {
+      return false;
+    }
   }
-  dbg(isPresent);
-  return isPresent.every((key) => key < 2);
+  
+  return  true;
 }
+
--- games/suduku/test/suduku_test.js
+++ games/suduku/test/suduku_test.js
@@ -2,17 +2,25 @@ import { assertEquals } from "@std/assert";
 import { validRow } from "../src/suduku_validation.js";
 
 Deno.test("Basic sample | valid Row", () => {
-  assertEquals(validRow([0, 3, 2, 1, 4],3), true);
+  assertEquals(validRow([
+  [1,2,3],
+  [2,3,1],
+  [3,1,2],
+],0), true);
 })
 
 Deno.test("double none value | valid Row", () => {
-  assertEquals(validRow([0, 3, 0, 1, 4],3), true);
+  assertEquals(validRow([
+  [1,0,0],
+  [2,3,1],
+  [0,1,2],
+],0), true);
 })
 
 Deno.test("dublicat value | valid Row", () => {
-  assertEquals(validRow([0, 3, 0, 1, 3],3), false);
-})
-
-Deno.test("no Value Present| valid row", () => {
-  assertEquals(validRow([0, 0, 0, 0, 0], 3), true);
+  assertEquals(validRow([
+  [1,2,3],
+  [2,1,1],
+  [3,1,2],
+],1), false);
 })
