6bc026a5143135bead21b68eb01a6dbb2b711876
Pradip Jana <janaprdip501@gmail.com>
2026-01-04 16:19:36 +0530

    create a common function for checking dublicate value

--- games/suduku/src/suduku_validation.js
+++ games/suduku/src/suduku_validation.js
@@ -5,19 +5,18 @@ const input = [
   [2,3,1],
   [3,1,2],
 ]
+export const hasDublicate = (values) => {
+  const seen = new Set();
+  for (const value of values) {
+    if (value === 0) continue;
+    if (seen.has(value)) return false;
+    seen.add(value);
+  }
+  return true;
+}
 
 export const validRow = (grid,y) => {
-  const isPresent = Array.from({ length: grid[0].length }, () => 0);
-
-  for (let x = 0; x < grid.length; x++) {
-    if (grid[y][x] === 0) continue;
-    isPresent[grid[y][x] - 1]++;
-    if (isPresent[grid[y][x] - 1] > 1) {
-      return false;
-    }
-  }
-  
-  return  true;
+  return hasDublicate(grid[y]);
 }
 
 export const validCol = (grid,x) => {
