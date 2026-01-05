df995a61bfd12800f00c6ceea5b64b73549e80fb
Pradip Jana <janaprdip501@gmail.com>
2026-01-05 19:25:49 +0530

    transfer the input data to json file

--- cool/table.js
+++ cool/table.js
@@ -6,18 +6,22 @@ const findHighestCol = (keys) =>
     "(idx)".length,
   );
 
-const makeTable = (data) => {
-  const firstColLength = findHighestCol(Object.keys(data)) + 2;
-  const secondColLength = findHighestCol(Object.values(data)) + 2;
+const printTable = (firstColLength, secondColLength) => {
   console.log("_".repeat((firstColLength + secondColLength) * 1.25));
-  console.log(`| (idx) | ${"Values".padEnd(secondColLength)} |`);
-  console.log("_".repeat((firstColLength + secondColLength) * 1.25));
+  console.log(`%c| (idx) | ${"Values".padEnd(secondColLength)} |`,'background-color: yellow');
+  console.log("%c" + "_".repeat((firstColLength + secondColLength) * 1.25),'background-color: yellow');
   for (const [key, value] of Object.entries(data)) {
     console.log(
       `| ${key.padEnd(firstColLength)}| ${value.padEnd(secondColLength)}|`,
     );
   }
   console.log("_".repeat((firstColLength + secondColLength) * 1.25));
+};
+
+const makeTable = (data) => {
+  const firstColLength = findHighestCol(Object.keys(data)) + 2;
+  const secondColLength = findHighestCol(Object.values(data)) + 2;
+  printTable(firstColLength,secondColLength)
 };
 
 makeTable(data);
