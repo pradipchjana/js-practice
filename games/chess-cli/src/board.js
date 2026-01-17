de167764229d2ab6a5bc716a341b30eb95eb0b58
Pradip Jana <janaprdip501@gmail.com>
2026-01-18 00:46:16 +0530

    add setboard and getboard functions and test cases for this

--- games/chess-cli/src/board.js
+++ games/chess-cli/src/board.js
@@ -2,3 +2,18 @@ export const createBoard = () => {
   const board = Array.from({ length: 8 }, () => Array(8).fill(null));
   return board;
 }
+
+export const getSquare = (board, file, rank) => {
+  const col = file.charCodeAt(0) - "a".charCodeAt(0);
+  const row = 8 - rank;
+  if (row < 0 || row > 7 || col < 0 || col > 7) return null;
+  return board[row][col];
+}
+
+export const setSquare = (board, file, rank,peice) => {
+  const col = file.charCodeAt(0) - "a".charCodeAt(0);
+  const row = 8 - rank;
+  if (row >= 0 && row < 8 && col >= 0 && col < 8) {
+    board[row][col] = peice;
+  }
+}
--- games/chess-cli/test/board_test.js
+++ games/chess-cli/test/board_test.js
@@ -1,6 +1,7 @@
-import { createBoard } from "../src/board.js";
+import { createBoard, setSquare,getSquare } from "../src/board.js";
 import { assertEquals } from "@std/assert";
-import { describe,it,beforeEach } from "@std/testing/bdd";
+import { beforeEach, describe, it } from "@std/testing/bdd";
+
 describe("test create board | create a 8*8 grid filled with null", () => {
   const board = createBoard();
   it("check the row length is 8", () => {
@@ -15,5 +16,20 @@ describe("test create board | create a 8*8 grid filled with null", () => {
   });
   it("check last element is null", () => {
     assertEquals(board[7][7], null);
+  });
+});
+
+describe("test getSquare and setSquare", () => {
+  const board = createBoard();
+  beforeEach(() => {
+    setSquare(board, "e", 4, "♙");
+    setSquare(board, "d", 5, "♟");
+  })
+  it("check sucessfully set 8", () => {
+    assertEquals(getSquare(board,"e",4),"♙");
+  });
+
+  it("check try to fetch null value| out of boundary", () => {
+    assertEquals(getSquare(board,"w",4), null);
   });
 });
